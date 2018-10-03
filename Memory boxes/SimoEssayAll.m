4a51c07b1bed2ca7d80526dee7cd8b75387b889d
Doerig Adrien <doerig@intranet.epfl.ch>
2018-10-03 13:58:24 +0200

    fixed small bug

--- Memory boxes/SimoEssayAll.m
+++ Memory boxes/SimoEssayAll.m
@@ -1,6 +1,22 @@
 % All, change one parameter at a time
 clear all; close all; clc;
 
+% directory management
+progPath = fileparts(which(mfilename)); % The program directory
+cd(progPath) % go there just in case we are far away
+addpath(genpath(progPath)); % add the folder and subfolders to path
+
+% if there's no results directory, create one
+if exist([progPath, '\results\'], 'dir') == 0
+    mkdir('results');
+end
+
+if exist([progPath, '\results\parameter_grid_search'], 'dir') == 0
+    cd('./results')
+    mkdir('parameter_grid_search');
+    cd(progPath)
+end
+
 % parameters necessary for plotOutputNoNDtimeChooseReadoutTime function
 tauIntegrate = 0.204876118312500;       % [s]
 tauDecay = 0.205257993312500;           % [s]
@@ -16,7 +32,7 @@ readoutTime = 0.45;
 for tauIntegrate = 0.1:0.1:1
     p = [tauIntegrate, tauDecay, wongWang_gain, wongWang_sigma, wongWang_mu0];
     %call function
-    plotOutputNoNDtimeChooseReadoutTime(p, dataType, subjectNumber, readoutTime, ['parameter_grid_search_', num2str(tauIntegrate)]);
+    plotOutputNoNDtimeChooseReadoutTime(p, dataType, subjectNumber, readoutTime, ['results/parameter_grid_search/tauIntegrate_', num2str(tauIntegrate)]);
 end
 tauIntegrate = 0.204876118312500;  
 
@@ -24,7 +40,7 @@ tauIntegrate = 0.204876118312500;
 for tauDecay = 0.1:0.1:1
     p = [tauIntegrate, tauDecay, wongWang_gain, wongWang_sigma, wongWang_mu0];
     %call function
-    plotOutputNoNDtimeChooseReadoutTime(p, dataType, subjectNumber, readoutTime, ['parameter_grid_search_', num2str(tauDecay)]);
+    plotOutputNoNDtimeChooseReadoutTime(p, dataType, subjectNumber, readoutTime, ['results/parameter_grid_search/tauDecay_', num2str(tauDecay)]);
 end
 tauDecay = 0.205257993312500;
 
@@ -32,7 +48,7 @@ tauDecay = 0.205257993312500;
 for wongWang_gain = 0.1:0.1:1
     p = [tauIntegrate, tauDecay, wongWang_gain, wongWang_sigma, wongWang_mu0];
     %call function
-    plotOutputNoNDtimeChooseReadoutTime(p, dataType, subjectNumber, readoutTime, ['parameter_grid_search_', num2str(wongWang_gain)]);
+    plotOutputNoNDtimeChooseReadoutTime(p, dataType, subjectNumber, readoutTime, ['results/parameter_grid_search/wongWang_gain_', num2str(wongWang_gain)]);
 end
 wongWang_gain = 0.204561344062500; 
 
@@ -40,7 +56,7 @@ wongWang_gain = 0.204561344062500;
 for wongWang_sigma = 0.1:0.1:1
     p = [tauIntegrate, tauDecay, wongWang_gain, wongWang_sigma, wongWang_mu0];
     %call function
-    plotOutputNoNDtimeChooseReadoutTime(p, dataType, subjectNumber, readoutTime, ['parameter_grid_search_', num2str(wongWang_sigma)]);
+    plotOutputNoNDtimeChooseReadoutTime(p, dataType, subjectNumber, readoutTime, ['results/parameter_grid_search/wongWang_sigma_', num2str(wongWang_sigma)]);
 end
 wongWang_sigma = 0.204811396750000;
 
@@ -48,7 +64,7 @@ wongWang_sigma = 0.204811396750000;
 for wongWang_mu0 = 0.1:0.1:1
     p = [tauIntegrate, tauDecay, wongWang_gain, wongWang_sigma, wongWang_mu0];
     %call function
-    plotOutputNoNDtimeChooseReadoutTime(p, dataType, subjectNumber, readoutTime,['parameter_grid_search_', num2str(wongWang_mu0)]);
+    plotOutputNoNDtimeChooseReadoutTime(p, dataType, subjectNumber, readoutTime,['results/parameter_grid_search/wongWang_mu0_', num2str(wongWang_mu0)]);
 end
 wongWang_mu0 = 0.204749724562500;
 
